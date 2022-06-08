815151601c82559f9bb0f6131d2c80ec477e24d1
Steve Wufeng <sw5874@rit.edu>
2022-06-07 21:34:15 -0400

    added install lib script to automatically install libraries

---
+++ install_lib.sh
@@ -0,0 +1,9 @@
+# install beautiful soup4
+pip install beautifulsoup4
+# install requests
+pip install requests
+
+# if it throws an error:
+# FeatureNotFound: Couldn't find a tree builder with the features you requested: lxml. Do you need to install a parser library?
+# REMOVE THE HASH TAG INFRONT OF THE FOLLOWING LINE
+# pip install lxml
